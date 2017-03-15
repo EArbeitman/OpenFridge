0b23ea9bf7ee846f38ae6057687c82ebbbd0ef1b
CLMBN <juanmiguel.ossa+git@gmail.com>
2017-03-15 15:25:57 -0400

    updated dynamic title to each of the search results

--- assets/javascript/search-results.js
+++ assets/javascript/search-results.js
@@ -1,7 +1,7 @@
 function populateResults (){
 for (var i = 0; i < searchResultsLabels.length; i++) {
 
-	var thumbnails = $('<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 thumbnail"><img src="' + searchResultsImages[i] + '" alt="..." href="' + searchResultsUrls[i] + '" target="_blank" /><div class="wrapper"><div class="youtube"><img src="assets/images/youtube.jpg" width="40vw" alt="..." href="' + youtubeEmbedArray[i] + '" target="_blank"" /></div><div class="caption post-content"><a href="' + searchResultsUrls[i] + '" target="_blank"><p class="title">Herb-Roasted Chickens</p></a></div></div></div>');
+	var thumbnails = $('<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 thumbnail"><img src="' + searchResultsImages[i] + '" alt="..." href="' + searchResultsUrls[i] + '" target="_blank" /><div class="wrapper"><div class="youtube"><img src="assets/images/youtube.jpg" width="40vw" alt="..." href="' + youtubeEmbedArray[i] + '" target="_blank"" /></div><div class="caption post-content"><a href="' + searchResultsUrls[i] + '" target="_blank"><p class="title">' + searchResultsLabels[i] + '</p></a></div></div></div>');
 
 	$("#resultsList").append(thumbnails);
 }
